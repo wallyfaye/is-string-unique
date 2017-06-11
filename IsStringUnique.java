4f4486a9e6ee233bc1417d7265596843d1a5e619
Wally Faye <wally.faye@gmail.com>
2017-06-11 08:33:31 -0400

    init

---
+++ IsStringUnique.java
@@ -0,0 +1,9 @@
+/**
+ * Determines if a string is unique
+ */
+
+class IsStringUnique{
+	public IsStringUnique(String sting_request){
+		System.out.println(sting_request);
+	}
+}
---
+++ _Demo.java
@@ -0,0 +1,6 @@
+class _Demo {
+	public static void main(String[] args){
+		System.out.println("_Demo running");
+		IsStringUnique string_uniq = new IsStringUnique("string to test");
+	}
+}
