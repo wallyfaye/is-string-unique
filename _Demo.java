09d023cd0631e5e89b13a28445f35924fece0104
Wally Faye <wally.faye@gmail.com>
2017-06-11 20:04:29 -0400

    accept arguments

--- _Demo.java
+++ _Demo.java
@@ -1,6 +1,10 @@
 class _Demo {
 	public static void main(String[] args){
-		StringUnique str1 = new StringUnique("test string");
+		if (args.length < 1){
+			System.out.println("Please specify a string to test");
+			System.exit(0);
+		}
+		StringUnique str1 = new StringUnique(args[0]);
 		String result = new Boolean(str1.res).toString();
 		System.out.println("The result of \"" + str1.req + "\" is: " + result);
 	}
