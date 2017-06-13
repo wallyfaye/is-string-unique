ebdaa6c243be2bb538872f3abb7b51dcb60e36a4
Wally Faye <wally.faye@gmail.com>
2017-06-12 23:25:37 -0400

    iterate through values

---
+++ HashTable.java
@@ -0,0 +1,10 @@
+class HashTable {
+
+	public HashTable() {
+	}
+
+	public void addData(char key) {
+		System.out.println(key);
+	}
+
+}
--- StringUnique.java
+++ StringUnique.java
@@ -4,6 +4,10 @@ class StringUnique{
 	public Boolean res;
 
 	public StringUnique(String str){
+		HashTable str_hash = new HashTable();
+		for(int i = 0; i < str.length(); i++){
+			str_hash.addData(str.charAt(i));
+		}
 		System.out.println(str);
 		req = str;
 		res = false;
