216e3690c2c3c2e6bf6830e1ed2ed11006d45f71
Wally Faye <wally.faye@gmail.com>
2017-06-14 22:47:48 -0400

    hashing

--- HashTable.java
+++ HashTable.java
@@ -1,10 +1,18 @@
 class HashTable {
 
+	private int array_size = 57;
+
 	public HashTable() {
+	}
+
+	public Integer hash(char key){
+		int char_num = Character.getNumericValue(key);
+		return char_num % array_size;
 	}
 
 	public boolean addData(char key) {
 		System.out.println(key);
+		System.out.println(hash(key));
 		return false;
 	}
 
